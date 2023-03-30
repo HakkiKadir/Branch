82c6857aa143dcfeb0909d0738655e0a14ae44a4
HakkiKadir <hakkikadirozbek@gmail.com>
2023-03-30 22:15:13 +0300

    Branch Kullanımı

--- src/B01.java
+++ src/B01.java
@@ -1,5 +1,20 @@
 public class B01 {
     public static void main(String[] args) {
         System.out.println("Branch Kullanımı");
+
+        /*
+        Branckh kullanımı
+        1- Bir proje üzerinde çalışırken yapılacak olan çalışmaların proejeyi
+        ya da son kullanıcıyo etkilememesi için farklı
+        alanlarda çalışmaların devam ettirilmesini sağlar
+        2-Branch genelde projede düzeltme/geliştirme/değişiklik yapma durumunda kullanılır
+        3-En önemli amaç çalışan projeyi riske atmamaktır.
+        4-Orjinalde çalışmak yerine  branchte çalışılır.
+        5-çalışma bitince Team Lead tarafından masterde merge edilir.
+        6- Çakışma durumunda ise Team Lead kontrol ederek karar verir
+            - Çakışmaların her ikisini de silebilir
+            - Birini silip birini tutabilir
+            - Her ikisini de çakışmayacak şekilde düzenleyip tutabilir
+         */
     }
 }
